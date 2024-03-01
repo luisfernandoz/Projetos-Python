6744884e02022b78a79d96e8927bf04b348dd708
Luis Fernando <luisfernandozucatofilho@gmail.com>
2024-03-01 09:49:05 -0300

    Add database creation and table creation queries

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ def connect_to_database():
             host="localhost",
             user="root",
             password="",
-            database="calendario"
         )
         print("Connected to the database")
         return mydb
@@ -17,12 +16,20 @@ def connect_to_database():
         print("Error connecting to the database:", err)
         return None
 
+mydb = connect_to_database()
+mycursor = mydb.cursor()
+
+mycursor.close()
+
 @app.route('/')
 def index():
     mydb = connect_to_database()
     if mydb:
         try:
             mycursor = mydb.cursor()
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS calendario")
+            mycursor.execute("USE calendario")
+            mycursor.execute("CREATE TABLE IF NOT EXISTS eventos (eve_id INT AUTO_INCREMENT PRIMARY KEY, eve_nome VARCHAR(255), eve_desc TEXT, eve_date CHAR(16))")
             mycursor.execute("SELECT * FROM eventos ORDER BY eve_date ASC")
             result = mycursor.fetchall()
             return render_template('index.html', events=result)
@@ -41,6 +48,7 @@ def deleteEvent(event_id):
     if mydb:
         try:
             mycursor = mydb.cursor()
+            mycursor.execute("USE calendario")
             mycursor.execute("DELETE FROM eventos WHERE eve_id = %s", (event_id,))
             mydb.commit()
             return jsonify({'status': 'success', 'message': 'Event deleted successfully.'})
@@ -63,6 +71,7 @@ def submit():
     if mydb:
         try:
             cursor = mydb.cursor()
+            cursor.execute("USE calendario")
             cursor.execute("INSERT INTO eventos (eve_nome, eve_desc, eve_date) VALUES (%s, %s, %s)", (title, description, date))
             mydb.commit()
             cursor.close()    
